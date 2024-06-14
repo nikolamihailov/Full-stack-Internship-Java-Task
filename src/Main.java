8f3eaa18306717ea6d9b81ee3d8ebc7b17ca1cdf
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-06-14 21:55:24 +0300

    examples to test code added to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("Mitsubishi", "Mirage", 15000, 10, 3);
+        //  Safety rating different from 1-5 will throw error
+        // Car car2 = new Car("Mitsubishi", "Mirage", 15000, 10, 30);
+        Motorcycle motorcycle = new Motorcycle("Triumph", "Tiger Sport 660", 10000, 10, 20);
+        CargoVan cargoVan = new CargoVan("Citroen", "Jumper", 20000, 15, 8);
+
+        LocalDate startDate = LocalDate.of(2024,6,3);
+        LocalDate returnDate = LocalDate.of(2024,6,13);
+
+        Invoice invoice = new Invoice(car, "John", "Doe", startDate, returnDate);
+        Invoice invoice2 = new Invoice(motorcycle, "Mary", "Johnson", startDate, returnDate);
+        Invoice invoice3 = new Invoice(cargoVan, "John", "Markson", startDate, returnDate);
+
+        invoice.report();
+        invoice2.report();
+        invoice3.report();
     }
 }
