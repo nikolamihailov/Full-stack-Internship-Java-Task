b99a7d2c226ba652785de42dd00d0298b5053156
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-06-15 19:05:02 +0300

    extracted repeating code into method

--- src/VehicleCostsCalculator.java
+++ src/VehicleCostsCalculator.java
@@ -1,15 +1,16 @@
 public class VehicleCostsCalculator{
+    public double dailyInsuranceCostWithAdditionOrDiscount = 0;
+    double dailyInsuranceCost;
+    double dailyInsuranceCostDiff;
+    double totalInsurance;
+    double dailyRentCost = 0;
+    double totalRent;
+    double earlyReturnRentDiscount = 0;
+    double earlyReturnInsuranceDiscount;
+    double total;
+
     public String getCostsInfo(Vehicle vehicle, int actualDays, int rentDays ){
         StringBuilder sb = new StringBuilder();
-        double dailyRentCost = 0;
-        double dailyInsuranceCost;
-        double dailyInsuranceCostWithAdditionOrDiscount = 0;
-        double dailyInsuranceCostDiff;
-        double totalRent;
-        double totalInsurance;
-        double earlyReturnRentDiscount = 0;
-        double earlyReturnInsuranceDiscount;
-        double total;
 
         if(actualDays<=7){
             dailyRentCost = vehicle.getDailyRentalCost();
@@ -29,26 +30,15 @@ public class VehicleCostsCalculator{
 
         if(vehicle instanceof Car){
             if(((Car) vehicle).getSafetyRating()==4 || ((Car) vehicle).getSafetyRating() ==5){
-                sb.append("Initial insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n");
-                dailyInsuranceCostWithAdditionOrDiscount = dailyInsuranceCost*0.9;
-                dailyInsuranceCostDiff = dailyInsuranceCost - dailyInsuranceCostWithAdditionOrDiscount;
-                sb.append("Insurance discount per day: ").append(this.formatNum("$", dailyInsuranceCostDiff)).append("\n");
-                sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCostWithAdditionOrDiscount)).append("\n\n");
-                totalInsurance = dailyInsuranceCostWithAdditionOrDiscount*actualDays;
+               sb.append( calcInsurance(0.9, actualDays));
             }else{
                 sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n\n");
             }
         }
 
-
         if(vehicle instanceof Motorcycle){
             if(((Motorcycle) vehicle).getDriversAge()<25){
-                sb.append("Initial insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n");
-                dailyInsuranceCostWithAdditionOrDiscount = dailyInsuranceCost*1.2;
-                dailyInsuranceCostDiff = dailyInsuranceCostWithAdditionOrDiscount - dailyInsuranceCost;
-                sb.append("Insurance addition per day: ").append(this.formatNum("$", dailyInsuranceCostDiff)).append("\n");
-                sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCostWithAdditionOrDiscount)).append("\n\n");
-                totalInsurance = dailyInsuranceCostWithAdditionOrDiscount*actualDays;
+                sb.append(calcInsurance(1.2, actualDays));
             }else{
                 sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n\n");
             }
@@ -56,13 +46,7 @@ public class VehicleCostsCalculator{
 
         if(vehicle instanceof CargoVan){
             if(((CargoVan) vehicle).getDriversExperienceInYears()>5){
-                sb.append("Initial insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n");
-                dailyInsuranceCostWithAdditionOrDiscount = dailyInsuranceCost*0.85;
-                dailyInsuranceCostDiff = dailyInsuranceCost - dailyInsuranceCostWithAdditionOrDiscount;
-                sb.append("Insurance discount per day: ").append(this.formatNum("$", dailyInsuranceCostDiff)).append("\n");
-                sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCostWithAdditionOrDiscount)).append("\n\n");
-                totalInsurance = dailyInsuranceCostWithAdditionOrDiscount*actualDays;
-
+                sb.append(calcInsurance(0.85, actualDays));
             }else{
                 sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n\n");
             }
@@ -78,7 +62,6 @@ public class VehicleCostsCalculator{
             sb.append("Early return discount for insurance: ").append(this.formatNum("$", earlyReturnInsuranceDiscount)).append("\n\n");
         }
 
-
         sb.append("Total rent: ").append(this.formatNum("$", totalRent - earlyReturnRentDiscount)).append("\n");
         sb.append("Total insurance: ").append(this.formatNum("$", totalInsurance)).append("\n");
 
@@ -90,5 +73,20 @@ public class VehicleCostsCalculator{
 
     public String formatNum(String currency,double num){
         return String.format("%s%.2f", currency, num);
+    }
+
+    public String calcInsurance(double insuranceReduceIncreasePercent, int actualDays){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Initial insurance per day: ").append(this.formatNum("$", dailyInsuranceCost)).append("\n");
+        dailyInsuranceCostWithAdditionOrDiscount = dailyInsuranceCost*insuranceReduceIncreasePercent;
+        dailyInsuranceCostDiff = Math.abs(dailyInsuranceCost - dailyInsuranceCostWithAdditionOrDiscount);
+        if(insuranceReduceIncreasePercent>1){
+            sb.append("Insurance addition per day: ").append(this.formatNum("$", dailyInsuranceCostDiff)).append("\n");
+        }else{
+            sb.append("Insurance discount per day: ").append(this.formatNum("$", dailyInsuranceCostDiff)).append("\n");
+        }
+        sb.append("Insurance per day: ").append(this.formatNum("$", dailyInsuranceCostWithAdditionOrDiscount)).append("\n\n");
+        totalInsurance = dailyInsuranceCostWithAdditionOrDiscount*actualDays;
+        return sb.toString();
     }
 }
